e8ca9f31d56e716906ad0424d67d38050f12ec38
League Student <league@iMac-23.attlocal.net>
2019-09-10 17:19:01 -0700

    Module 0 Complete

---
+++ src/_03_morning_zombie/MorningZombie.java
@@ -0,0 +1,9 @@
+package _03_morning_zombie;
+
+import javax.swing.JOptionPane;
+
+public class MorningZombie {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null,"Make your bed, eat breakfast, brush teeth");
+}
+}
---
+++ src/_06_everything_is_awesome/EverythingIsAwesome.java
@@ -0,0 +1,11 @@
+package _06_everything_is_awesome;
+
+import javax.swing.JOptionPane;
+
+public class EverythingIsAwesome {
+public static void main(String[] args) {
+	String answer = 
+	JOptionPane.showInputDialog("what is awesome?"); 
+	JOptionPane.showMessageDialog(null, answer + " is awesome");
+}
+}
